d459e3a75ede8de8eff4fd08f34fd711eea2af06
fernando <fernando@fernando-debian>
2023-03-01 16:28:12 -0300

    testando filtros

--- script.js
+++ script.js
@@ -471,6 +471,8 @@ var onLoadedPosts = (res) => {
         loadPost(post, false);
     });
     loaded = true;
+    if(window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 100)
+        window.onscroll();
 };
 
 var imageFullScreen = (evt) => {
