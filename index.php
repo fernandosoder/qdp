6b3d0d2c9e65014fac65d3b2b45a84d0daebe0b7
fernando <fernando@fernando-debian>
2023-03-02 11:19:12 -0300

    Tentativa de internacionalização

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ header('Link: <' . $rootUrl . 'script.js?' . $v . '>; rel=preload; as=script', f
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" type="image/png" href="/logo.png">
+        <link href="<?php echo $rootUrl; ?>strings/en_us.css?<?= $v ?>" rel="stylesheet" />
         <link href="<?php echo $rootUrl; ?>css/style.css?<?= $v ?>" rel="stylesheet" />
         <script src="<?php echo $rootUrl; ?>script.js?<?= $v ?>" type="text/javascript" defer></script>
     </head>
