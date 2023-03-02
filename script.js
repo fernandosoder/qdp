4141fe176f5531bb7140e2fa1b098c93141389ea
fernando <fernando@fernando-debian>
2023-03-02 11:57:14 -0300

    Tentativa de internacionalização

--- script.js
+++ script.js
@@ -444,7 +444,10 @@ var updatePostData = (author, permlink, timeout = 5000) => {
 //            console.log(downvotes);
             upvote.classList.toggle("myvote", myvote > 0);
             downvote.classList.toggle("myvote", myvote < 0);
-            upvote.querySelector("output:first-child").innerHTML = upvotes + " votes";
+            let spanvotes = document.createElement("span");
+            spanvotes.classList.add("span_votes");
+            upvote.querySelector("output:first-child").innerHTML = upvotes;
+            upvote.querySelector("output:first-child").append(spanvotes);
             upvote.querySelector("output:last-child").innerHTML = "$ " + (upower / reward_fund.recent_claims * reward_fund.reward_balance.split(" ")[0] * median_price.base.split(" ")[0]).toFixed(3);
             downvote.querySelector("output:first-child").innerHTML = downvotes + " votes";
             downvote.querySelector("output:last-child").innerHTML = "$ " + (dpower / reward_fund.recent_claims * reward_fund.reward_balance.split(" ")[0] * median_price.base.split(" ")[0]).toFixed(3);
